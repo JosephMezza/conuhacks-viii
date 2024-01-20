25dd7f3c2eae3b69e43ec0a6a2884c9aae2e1e05
Andre <psnmeee@gmail.com>
2024-01-20 13:59:10 -0500

    first commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+public float speed = 0.5f;
+private Rigidbody2D rb;
+private Vector2 input;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        input.Normalize();
+    }
+
+    private void FixedUpdate(){
+        rb.velocity = input * speed;
+    }
+}
